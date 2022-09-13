ad73a761504e28dde25b2f3a43095a9aa69ab031
abdulshakir47 <112155594+abdulshakir47@users.noreply.github.com>
2022-09-13 10:30:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Reading stream and Writing stream
+
+
+
+const fs = require('fs') ;
+const readStream = fs.createReadStream ("./docs/blog3.txt",{encoding:'utf-8'})
+const writeStream = fs.createWriteStream("./docs/blog4.txt")
+readStream.on('data', (chunk) => {
+ console.log ('NEW CHUNK') ;
+ console.log (chunk) ;
+ writeStream.write(' New Chunk')
+ writeStream.write(chunk)
+});
+
